10eedc6b6742dbf0a236b7f382a1305594b3bc8c
CubeGameLP <mikakuns.2003@gmail.com>
2024-10-16 12:38:27 +0200

    add Item DTO

---
+++ Backend/EPSI/EPSI/DTOs/ItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace EPSI.DTOs
+{
+    public class ItemDTO
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string RoomName { get; set; }
+        public string RoomID { get; set; }
+        public string ShelfName { get; set; }
+        public string ShelfID { get; set; }
+        public int ItemAmount { get; set; }
+    }
+}

ddfbb0d61dbf39ad3ddbfebbac961402c0b06873
Gothi <132581790+Janboaaa@users.noreply.github.com>
2024-10-16 16:09:55 +0200

    add ItemDTO

---
+++ Frontend/WebApp/DTOs/ItemDTO.cs
@@ -0,0 +1,10 @@
+﻿public class ItemDTO
+{
+    public int ItemID { get; set; }
+    public required string ItemName { get; set; }
+    public required string RoomName { get; set; }
+    public required string RoomID { get; set; }
+    public required string ShelfName { get; set; }
+    public required string ShelfID { get; set; }
+    public int ItemAmount { get; set; }
+}
--- Frontend/WebApp/Program.cs
+++ Frontend/WebApp/Program.cs
@@ -1,11 +1,11 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddMudServices(); 
+builder.Services.AddMudServices();
+
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 

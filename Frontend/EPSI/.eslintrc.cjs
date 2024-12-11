4b6702a734c55d7a7244629027c26178c773dbee
CubeGameLP <mikakuns.2003@gmail.com>
2024-12-11 11:05:34 +0100

    👍

--- Frontend/EPSI/.eslintrc.cjs
+++ Frontend/EPSI/.eslintrc.cjs
@@ -46,6 +46,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/singleline-html-element-content-newline': 'off',
     'vue/multiline-html-element-content-newline': 'off',
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': 'off',
   },
 };
